892a5956a29cb6cc9c8a3ac8f7bf3d6f444b5486
Brandon Blaylock <brandon@null.pub>
2022-05-29 16:30:41 -0700

    fix: extend secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   brandon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH26soDnOC7jeIiaG65Vur93mFRbQipNZiWHd95ZF+TB brandon@neumann";
-  users = [ brandon ];
+  shared = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBajlplZeTASyUqnPJVrmkX3eyWT5I3JhwEWiDsV7NH1 brandon@null.pub";
+  users = [ brandon shared ];
 in
 {
   "vaultwarden.age".publicKeys = users;
