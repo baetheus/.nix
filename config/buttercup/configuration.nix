2fc1be10e66d42179e9275f1994888c1954f6911
Brandon Blaylock <brandon@null.pub>
2022-05-30 18:55:41 -0700

    feat: add nzbget to buttercup

--- config/buttercup/configuration.nix
+++ config/buttercup/configuration.nix
@@ -20,7 +20,7 @@
       isSystemUser = true;
     };
     groups.media = {
-      members = [ "plex" ];
+      members = [ "plex" "media" ];
     };
   };
 
@@ -53,11 +53,64 @@
     };
   };
 
-  # Services
+  # Plex Media Server
   services.plex = {
     enable = true;
     user = "plex";
     group = "media";
     dataDir = "/media";
+  };
+
+  services.nzbget = {
+    enable = true;
+    user = "nzbget";
+    group = "media";
+    settings = rec {
+      MainDir = "/var/lib/nzbget";
+      DestDir = "${MainDir}/dst";
+      InterDir = "${MainDir}/inter";
+      NzbDir = "${MainDir}/nzb";
+      QueueDir = "${MainDir}/queue";
+      TempDir = "${MainDir}/tmp";
+      ScriptDir = "${MainDir}/scripts";
+      LockFile = "${MainDir}/nzbget.lock";
+      LogFile = "/var/log/nzbget.log";
+      WebDir = "/var/www/nzbget.null.pub";
+      ConfigTemplate = "/usr/local/share/nzbget/nzbget.conf";
+      CertStore = pkgs.cacert;
+
+      # Easynews Europe
+      "Server1.Active" = "yes";
+      "Server1.Level" = "0";
+      "Server1.Optional" = "no";
+      "Server1.Group" = "0";
+      "Server1.Host" = "secure-eu.news.easynews.com";
+      "Server1.Port" = "443";
+      "Server1.Username" = "user";
+      "Server1.Password" = "pass";
+      "Server1.JoinGroup" = "no";
+      "Server1.Encryption" = "yes";
+      "Server1.Connections" = "10";
+      "Server1.Retention" = "5037";
+      "Server1.IpVersion" = "ipv4";
+      "Server1.Notes" = "Easynews European Secure Server";
+
+      # Easynews USA
+      "Server2.Active" = "yes";
+      "Server2.Level" = "0";
+      "Server2.Optional" = "no";
+      "Server2.Group" = "0";
+      "Server2.Host" = "secure-us.news.easynews.com";
+      "Server2.Port" = "443";
+      "Server2.Username" = "user";
+      "Server2.Password" = "pass";
+      "Server2.JoinGroup" = "no";
+      "Server2.Encryption" = "yes";
+      "Server2.Connections" = "10";
+      "Server2.Retention" = "5037";
+      "Server2.IpVersion" = "ipv4";
+      "Server2.Notes" = "Easynews American Secure Server";
+    };
+
   };
 }
