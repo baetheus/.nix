8744a879b00069e1d9e31ef3d1f6d8c024cedd58
Brandon Blaylock <brandon@null.pub>
2022-06-06 18:15:09 -0700

    feat: change window manager for remote

--- config/bubbles/configuration.nix
+++ config/bubbles/configuration.nix
@@ -69,12 +69,32 @@
 
         imports = [ ../users.nix ];
 
-        services.xserver.enable = true;
-        services.xserver.displayManager.sddm.enable = true;
-        services.xserver.desktopManager.plasma5.enable = true;
+
+        services.xserver = {
+          enable = true;
+
+          desktopManager = {
+            xterm.enable = false;
+          };
+
+          displayManager = {
+            defaultSession = "none+i3";
+          };
+
+          windowManager.i3 = {
+            enable = true;
+            extraPackages = with pkgs; [
+              dmenu #application launcher most people use
+              i3status # gives you the default i3 status bar
+              i3lock #default i3 screen locker
+              i3blocks #if you are planning on using i3blocks over i3status
+              firefox
+            ];
+          };
+        };
 
         services.xrdp.enable = true;
-        services.xrdp.defaultWindowManager = "startplasma-x11";
+        services.xrdp.defaultWindowManager = "i3";
         services.xrdp.openFirewall = true;
       };
     };
