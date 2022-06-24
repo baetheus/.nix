0fb7ce7fdbc12c68d7c69ef42d82c930d0cc8a61
Brandon Blaylock <brandon@null.pub>
2022-06-24 15:32:09 -0700

    feat: remove container from bubbles

--- configs/bubbles/configuration.nix
+++ configs/bubbles/configuration.nix
@@ -61,41 +61,41 @@
   };
 
   # Containers
-  containers.remote =
-    {
-      autoStart = true;
-      config = { config, pkgs, ... }: {
-        system.stateVersion = "22.05";
+  # containers.remote =
+  #   {
+  #     autoStart = true;
+  #     config = { config, pkgs, ... }: {
+  #       system.stateVersion = "22.05";
 
-        imports = [ ../users.nix ];
+  #       imports = [ ../users.nix ];
 
 
-        services.xserver = {
-          enable = true;
+  #       services.xserver = {
+  #         enable = true;
 
-          desktopManager = {
-            xterm.enable = false;
-          };
+  #         desktopManager = {
+  #           xterm.enable = false;
+  #         };
 
-          displayManager = {
-            defaultSession = "none+i3";
-          };
+  #         displayManager = {
+  #           defaultSession = "none+i3";
+  #         };
 
-          windowManager.i3 = {
-            enable = true;
-            extraPackages = with pkgs; [
-              dmenu #application launcher most people use
-              i3status # gives you the default i3 status bar
-              i3lock #default i3 screen locker
-              i3blocks #if you are planning on using i3blocks over i3status
-              firefox
-            ];
-          };
-        };
+  #         windowManager.i3 = {
+  #           enable = true;
+  #           extraPackages = with pkgs; [
+  #             dmenu #application launcher most people use
+  #             i3status # gives you the default i3 status bar
+  #             i3lock #default i3 screen locker
+  #             i3blocks #if you are planning on using i3blocks over i3status
+  #             firefox
+  #           ];
+  #         };
+  #       };
 
-        services.xrdp.enable = true;
-        services.xrdp.defaultWindowManager = "i3";
-        services.xrdp.openFirewall = true;
-      };
-    };
+  #       services.xrdp.enable = true;
+  #       services.xrdp.defaultWindowManager = "i3";
+  #       services.xrdp.openFirewall = true;
+  #     };
+  #   };
 }
