16bf08077776ee6c48993e09da2a65c357bca44c
Brandon Blaylock <brandon@null.pub>
2022-06-28 10:43:43 -0700

    feat: try recompiling zfs with mail support and no smtp

--- configs/zfs.nix
+++ configs/zfs.nix
@@ -1,40 +1,20 @@
-{ config, pkgs, ... }: {
+{ config, nixpkgs, ... }: {
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.devNodes = "/dev/";
 
   services.zfs.autoScrub.enable = true;
   services.zfs.autoSnapshot.enable = true;
 
-  age.secrets.msmtp-passwordeval.file = ../secrets/msmtp-passwordeval.age;
-
-  # Setup SMTP Relay
-  programs.msmtp = {
-    enable = true;
-    setSendmail = true;
-    defaults = {
-      aliases = "/etc/aliases";
-      port = 465;
-      tls_trust_file = "/etc/ssl/certs/ca-certificates.crt";
-      tls = "on";
-      auth = "login";
-      tls_starttls = "off";
-    };
-    accounts = {
-      default = {
-        host = "smtp.fastmail.com";
-        passwordeval = "cat ${config.age.secrets.msmtp-passwordeval.path}";
-        user = "noreply@null.pub";
-        from = "noreply@null.pub";
-      };
-    };
+  nixpkgs.config.packageOverrides = pkgs: {
+    zfsStable = pkgs.zfsStable.override { enableMail = true; };
   };
 
   # Tell zfs zed to use msmtp relay
+  services.zfs.zed.enableMail = true;
   services.zfs.zed.settings = {
     ZED_DEBUG_LOG = "/tmp/zed.debug.log";
     ZED_EMAIL_ADDR = [ "brandon@null.pub" ];
-    ZED_EMAIL_PROG = "${pkgs.msmtp}/bin/msmtp";
-    ZED_EMAIL_OPTS = "-s '@SUBJECT@' @ADDRESS@'";
+    ZED_EMAIL_OPTS = "-r 'noreply@null.pub' -s '@SUBJECT@' @ADDRESS@";
 
     ZED_NOTIFY_INTERVAL_SECS = 3600;
     ZED_NOTIFY_VERBOSE = true;
@@ -42,5 +22,4 @@
     ZED_USE_ENCLOSURE_LEDS = true;
     ZED_SCRUB_AFTER_RESILVER = true;
   };
-  services.zfs.zed.enableMail = false;
 }
