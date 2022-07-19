29b5ba38d093939e3dbc9b6102929f92cac35fdf
Brandon Blaylock <brandon@null.pub>
2022-07-19 15:59:48 -0700

    feat: update common configurations

--- configs/common.nix
+++ configs/common.nix
@@ -1,27 +1,35 @@
 { pkgs, ... }: {
   # Nix
   nix = {
-    binaryCaches = [
-      "https://cache.nixos.org/"
-    ];
-
-    binaryCachePublicKeys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    ];
-
     package = pkgs.nixUnstable;
 
-    trustedUsers = [ "@wheel" ];
+    settings = {
+      trustedUsers = [ "@wheel" ];
+      substituters = [
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+
+      auto-optimise-store = true;
+    };
+
+    gc = {
+      automatic = true;
+
+    };
 
     extraOptions = ''
       experimental-features = nix-command flakes
-      auto-optimise-store = true
       keep-outputs = true
       keep-derivations = true
     '';
   };
 
+  # We like one timezone to rule them all, might as well be mine
   time.timeZone = "America/Los_Angeles";
 
+  # Just the basics
   environment.systemPackages = with pkgs; [ vim wget git ];
 }
