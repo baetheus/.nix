6948ba23a866a6c3832f8109c123a21b9c97563c
Brandon Blaylock <brandon@null.pub>
2022-08-12 17:55:38 -0700

    feat: add headscale to abigail

--- systems/abigail.nix
+++ systems/abigail.nix
@@ -10,4 +10,23 @@
   networking.hostId = "a9a768fa";
   networking.firewall.allowedTCPPorts = [ 22 ];
 
+  # Nginx
+  security.acme.acceptTerms = true;
+  security.acme.defaults.email = "admin@null.pub";
+
+  # Headscale
+  services.headscale = {
+    enable = true;
+    address = "0.0.0.0";
+    port = 443;
+    serverUrl = "https://net.null.pub";
+    tls.letsencrypt.hostname = "net.null.pub";
+
+    dns = {
+      magicDns = true;
+      nameservers = [ "1.1.1.1" ];
+      domains = [ "null.internal" ];
+      baseDomain = "null.internal";
+    };
+  };
 }
