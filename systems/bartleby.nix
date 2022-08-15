cfe48fc13310880b8a3870bc4e8045dea9042d23
Brandon Blaylock <brandon@null.pub>
2022-08-15 13:25:44 -0700

    feat: initial media service on bartleby

--- systems/bartleby.nix
+++ systems/bartleby.nix
@@ -8,6 +8,122 @@
 
   networking.hostName = "bartleby";
   networking.hostId = "1de212b7";
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedTCPPorts = [ 22 80 443 32400 ];
 
+  # Secrets
+  age.secrets.basicauth = {
+    file = ../secrets/basicauth.age;
+    owner = "nginx";
+    group = "nginx";
+  };
+
+  # Users and Groups
+  users = {
+    users.media = {
+      group = "media";
+      isSystemUser = true;
+    };
+    groups.media = {
+      members = [ "media" ];
+    };
+  };
+
+  # Nginx
+  security.acme.acceptTerms = true;
+  security.acme.defaults.email = "admin@null.pub";
+
+  services.nginx = {
+    enable = true;
+
+    virtualHosts = {
+      "bartleby.null.pub" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          root = "/var/www/bartleby.null.pub";
+          extraConfig = "autoindex on;";
+        };
+      };
+
+      "plex.lkj.one" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://0.0.0.0:32400";
+          proxyWebsockets = true;
+        };
+      };
+
+      "nzbget.lkj.one" = {
+        forceSSL = true;
+        enableACME = true;
+        basicAuthFile = config.age.secrets.basicauth.path;
+        locations."/" = {
+          proxyPass = "http://0.0.0.0:6789";
+          proxyWebsockets = true;
+        };
+      };
+
+      "series.lkj.one" = {
+        forceSSL = true;
+        enableACME = true;
+        basicAuthFile = config.age.secrets.basicauth.path;
+        locations."/" = {
+          proxyPass = "http://0.0.0.0:8989";
+          proxyWebsockets = true;
+        };
+      };
+
+      "movies.lkj.one" = {
+        forceSSL = true;
+        enableACME = true;
+        basicAuthFile = config.age.secrets.basicauth.path;
+        locations."/" = {
+          proxyPass = "http://0.0.0.0:7878";
+          proxyWebsockets = true;
+        };
+      };
+
+      "music.lkj.one" = {
+        forceSSL = true;
+        enableACME = true;
+        basicAuthFile = config.age.secrets.basicauth.path;
+        locations."/" = {
+          proxyPass = "http://0.0.0.0:8686";
+          proxyWebsockets = true;
+        };
+      };
+    };
+  };
+
+  # Media Services
+  services.plex = {
+    enable = true;
+    user = "media";
+    group = "media";
+  };
+
+  services.nzbget = {
+    enable = true;
+    user = "media";
+    group = "media";
+  };
+
+  services.sonarr = {
+    enable = true;
+    user = "media";
+    group = "media";
+  };
+
+  services.radarr = {
+    enable = true;
+    user = "media";
+    group = "media";
+  };
+
+  services.lidarr = {
+    enable = true;
+    user = "media";
+    group = "media";
+  };
 }
