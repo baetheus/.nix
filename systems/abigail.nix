1e44f2ed105ea8f6e283186d0748612a96aebd84
Brandon Blaylock <brandon@null.pub>
2022-08-19 08:57:01 -0700

    feat: just use tailscale since ios client is hard

--- systems/abigail.nix
+++ systems/abigail.nix
@@ -30,14 +30,6 @@
         };
       };
 
-      "net.null.pub" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8080";
-        };
-      };
-
       "vault.null.pub" = {
         forceSSL = true;
         enableACME = true;
@@ -49,21 +41,6 @@
           proxyWebsockets = true;
         };
       };
-    };
-  };
-
-  # Headscale
-  environment.systemPackages = with pkgs; [ headscale ];
-
-  services.headscale = {
-    enable = true;
-    serverUrl = "https://net.null.pub";
-
-    dns = {
-      magicDns = true;
-      nameservers = [ "1.1.1.1" ];
-      domains = [ ];
-      baseDomain = "null.pub";
     };
   };
 
