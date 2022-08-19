538f4f40564808d3df31c472967557e5fc757543
Brandon Blaylock <brandon@null.pub>
2022-08-19 09:12:57 -0700

    feat: add darkhttpd to common/minimal

--- systems/common/minimal.nix
+++ systems/common/minimal.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   # Minimal Config
   time.timeZone = "America/Los_Angeles";
-  environment.systemPackages = with pkgs; [ vim wget git zellij ];
+  environment.systemPackages = with pkgs; [ darkhttpd vim wget git zellij ];
 }
