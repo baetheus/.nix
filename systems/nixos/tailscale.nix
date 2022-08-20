02a147b7396ff3c175f7883a61da90f2edef811f
Brandon Blaylock <brandon@null.pub>
2022-08-20 14:20:22 -0700

    feat: allow packet forwarding for tailscale exits

--- systems/nixos/tailscale.nix
+++ systems/nixos/tailscale.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }: {
   services.tailscale.enable = true;
   environment.systemPackages = with pkgs; [ tailscale ];
+
+  # Allow package forwarding
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
+
   networking.firewall.checkReversePath = "loose";
 }
