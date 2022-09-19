b85fa7714d42ea35f9aa2b3e608df95427f4583a
Brandon Blaylock <brandon@null.pub>
2022-09-19 16:31:07 -0700

    feat: implement headscale on abigail

--- systems/abigail.nix
+++ systems/abigail.nix
@@ -30,6 +30,14 @@
         };
       };
 
+      "net.null.pub" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8080";
+        };
+      };
+
       "vault.null.pub" = {
         forceSSL = true;
         enableACME = true;
@@ -41,6 +49,21 @@
           proxyWebsockets = true;
         };
       };
+    };
+  };
+
+  # Headscale
+  environment.systemPackages = with pkgs; [ headscale ];
+
+  services.headscale = {
+    enable = true;
+    serverUrl = "https://net.null.pub";
+
+    dns = {
+      magicDns = true;
+      nameservers = [ "1.1.1.1" ];
+      domains = [ "net.internal" ];
+      baseDomain = "net.internal";
     };
   };
 
