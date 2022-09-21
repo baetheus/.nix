a4b62298a5fa066fbb0c4ea937dcf9e69d8bcce8
Brandon Blaylock <brandon@null.pub>
2022-09-21 11:18:11 -0700

    fix: correct import of innernet in sys bundle

--- systems/bundles/sys-1-sat-32.nix
+++ systems/bundles/sys-1-sat-32.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }: {
   imports = [
     ../hardware/sys-1-sat-32.nix
+    ../modules/innernet.nix
     ../common/minimal.nix
-    ../common/innernet.nix
     ../nixos/zfs.nix
     ../nixos/common.nix
     ../nixos/age.nix
