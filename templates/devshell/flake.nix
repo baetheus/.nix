55fc95da405662b913496f6c769ced5d0373dd91
Brandon Blaylock <brandon@null.pub>
2022-09-22 15:23:01 -0700

    fix: correct refs to utils

--- templates/devshell/flake.nix
+++ templates/devshell/flake.nix
@@ -6,21 +6,20 @@
   inputs.devshell.url = "github:numtide/devshell";
 
   outputs = { self, nixpkgs, utils, devshell  }:
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    overlays = [ devshell.overlay ];
-    pkgs = import nixpkgs { inherit system overlays; };
-    shell = with pkgs.devshell; mkShell {
-      imports = [
-        (importTOML ./devshell.toml)
-      ];
-      # Packages go here ?
-      packages = [];
-    };
-  in {
-    devShells = {
-      default = shell;
-    };
-  });
+    utils.lib.eachDefaultSystem (system: let
+      overlays = [ devshell.overlay ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      shell = with pkgs.devshell; mkShell {
+        imports = [
+          (importTOML ./devshell.toml)
+        ];
+        # Packages go here ?
+        packages = [];
+      };
+    in {
+      devShells = {
+        default = shell;
+      };
+    });
 }
 
--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -2,10 +2,10 @@
   description = "A simple flake";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
       shell = with pkgs; mkShell {
         # Insert Packages Here
