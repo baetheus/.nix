013eba83736cb15dad327758ef5cc184ebd20160
Brandon Blaylock <brandon@null.pub>
2022-09-26 17:13:08 -0700

    feat: add window manager to toph

--- systems/toph.nix
+++ systems/toph.nix
@@ -12,6 +12,8 @@
   system.stateVersion = "22.05"; # Did you read the comment?
 
 
+
+
   # Networking
   networking.hostName = "toph"; # Define your hostname.
   networking.hostId = "007f0200";
@@ -20,7 +22,7 @@
   # Firewall
   networking.firewall.enable = true;
   networking.firewall.allowedUDPPorts = [ 22 53 631 5353 ];
-  networking.firewall.allowedTCPPorts = [ 22 53 631 3478 5353 41641 ];
+  networking.firewall.allowedTCPPorts = [ 22 53 631 3478 3389 5353 41641 ];
 
   # Printing
   services.printing.enable = true;
@@ -36,4 +38,12 @@
   services.avahi.publish.domain = true;
   services.avahi.publish.userServices = true;
 
+  # Window Manager
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # RDP
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "startplasma-x11";
 }
