441fe1548da3708790f56747299798c3f1f42c4e
Brandon Blaylock <brandon@null.pub>
2022-09-28 17:02:52 -0700

    feat: add new yubikey config

--- homes/profiles.nix
+++ homes/profiles.nix
@@ -11,10 +11,12 @@
     email = "brandon@null.pub";
     signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKApq/9Vfhbk1M5wu0XmnajPCl/KlrL4bW6llSjReniR";
     keys = [
-      # Keychain Yubkey
+      # Keychain Yubkey A
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAetuhFZ8SCOLnYdfZOCFTQLzIh3a25WX991X5aWem5eAAAAC3NzaDpkZWZhdWx0 brandon@rosalind"
-      # Folder Yubikey
+      # Folder Yubikey B
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIO1pi4MnWUTF2w9GBbxk7F5uuYmt+uRA7gKMGuKqeQe3AAAAC3NzaDpkZWZhdWx0 brandon@rosalind"
+      # Laptop Yubikey C
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIL7W3Bg5SHwsLQqOjL3lQWf2F9zqY19g9MusuKXi93VtAAAAC3NzaDpkZWZhdWx0 brandon@rosalind"
     ];
   };
 
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
   keychain-a = "age1yubikey1q0w4elvpyp83lnat0hce5247rvuvmjnx2d670t0qp07447rxqyulx7tpv2r";
   folder-b = "age1yubikey1qwqyr54w5yseu8lwusqr9tvxm8e30tv7mn3xf4swjaq6r383985k5t7fp3y";
+  laptop-c = "age1yubikey1qf95j5rtmv2tunv0a4f2qecnej37zsegy840g38aq5hvzcehv2d6jqpyvzw";
   shared = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBajlplZeTASyUqnPJVrmkX3eyWT5I3JhwEWiDsV7NH1 brandon@null.pub";
-  all = [ keychain-a folder-b shared ];
+  all = [ keychain-a folder-b laptop-c shared ];
 in
 {
   "vaultwarden.age".publicKeys = all;
