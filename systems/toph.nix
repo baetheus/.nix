0d4fcbad2d9a8a8f009905adb8d59520dd7da7f7
Brandon Blaylock <brandon@null.pub>
2022-10-03 16:09:08 -0700

    feat: make toph wireless

--- systems/toph.nix
+++ systems/toph.nix
@@ -8,33 +8,28 @@
     ./nixos/openssh.nix
   ];
 
+  # Secrets
+  age.secrets."tuna-wifi".file = ../secrets/tuna-wifi.age;
+
   # General
   system.stateVersion = "22.05"; # Did you read the comment?
-  environment.systemPackages = with pkgs; [
-    firefox
-  ];
-
-  # Audio
-  sound.enable = true;
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.settings = {
-    General = {
-      Enable = "Control,Gateway,Headset,Media,Sink,Socket,Source";
-      MultiProfile = "multiple";
-    };
-  };
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.package = pkgs.pulseaudioFull;
 
   # Networking
   networking.hostName = "toph"; # Define your hostname.
   networking.hostId = "007f0200";
   networking.interfaces.eno1.useDHCP = true;
+  networking.wireless.enable = true;
+  networking.interfaces.wlp0s20f3.useDHCP = true;
+  networking.supplicant = {
+    "WLAN" = {
+      configFile.path = config.age.secrets."tuna-wifi".path;
+    };
+  };
 
   # Firewall
   networking.firewall.enable = true;
   networking.firewall.allowedUDPPorts = [ 22 53 631 5353 ];
-  networking.firewall.allowedTCPPorts = [ 22 53 631 3478 3389 5353 41641 ];
+  networking.firewall.allowedTCPPorts = [ 22 53 631 5353 ];
 
   # Printing
   services.printing.enable = true;
@@ -49,30 +44,5 @@
   services.avahi.publish.enable = true;
   services.avahi.publish.domain = true;
   services.avahi.publish.userServices = true;
-
-  # Window Manager
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.autoLogin.relogin = true;
-  services.xserver.displayManager.sddm.settings = {
-    Autologin = {
-      Session = "plasma.desktop";
-      User = "brandon";
-    };
-  };
-
-  # Synergy
-  services.synergy.client = {
-    enable = true;
-    serverAddress = "192.168.86.21";
-  };
-
-  # Plex
-  services.plex = {
-    enable = true;
-    openFirewall = true;
-  };
 }
 
