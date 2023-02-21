121b9f0d17da94cdecdb08e1d9fe90df6529413f
Brandon Blaylock <brandon@null.pub>
2023-02-21 08:25:34 -0800

    fix: agenix changed module export

--- systems/default.nix
+++ systems/default.nix
@@ -88,7 +88,7 @@ in
       system = "x86_64-linux";
       modules = [
         hm-nixos
-        agenix.nixosModule
+        agenix.nixosModule.age
         self.homes.brandon.server
         ./toph.nix
       ];
@@ -100,7 +100,7 @@ in
       system = "x86_64-linux";
       modules = [
         hm-nixos
-        agenix.nixosModule
+        agenix.nixosModule.age
         self.homes.brandon.server
         ./abigail.nix
       ];
@@ -112,7 +112,7 @@ in
       system = "x86_64-linux";
       modules = [
         hm-nixos
-        agenix.nixosModule
+        agenix.nixosModule.age
         self.homes.brandon.server
         ./bartleby.nix
       ];
