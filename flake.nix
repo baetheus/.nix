c0106d07e910930f7949050f1172b27c1d8fc60e
Brandon Blaylock <brandon@null.pub>
2023-03-08 10:32:55 -0800

    feat: update to 22.11 branch

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
 
-    nixpkgs.url = "github:nixos/nixpkgs/22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/22.11";
 
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nix-darwin.url = "github:lnl7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     agenix.url = "github:ryantm/agenix/main";
