98d58f03e9f7e88741f9192b773ed83676a31003
Brandon Blaylock <brandon@null.pub>
2023-10-18 15:38:26 -0700

    fix: add rust-overlay var to rust flake inputs

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -11,13 +11,13 @@
     };
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, rust-overlay }:
     utils.lib.eachDefaultSystem (system: let
       overlays = [ (import rust-overlay) ];
       pkgs = import nixpkgs { inherit system overlays; };
       rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       shell = with pkgs; mkShell {
-        buildInputs = [ rustToolchain sqlite ];
+        buildInputs = [ rustToolchain ];
         # Insert Packages Here
         packages = [];
       };
