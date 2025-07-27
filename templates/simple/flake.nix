208913b53ed78b1f1c18150b1ed13fa8b05b768f
Brandon Blaylock <brandon@null.pub>
2025-07-27 10:28:20 -0700

    feat: update simple flake template

--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -1,15 +1,25 @@
 {
-  description = "A simple flake";
+  description = "CHANGE ME";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
   inputs.utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
+      mkScript = pkgs.writeShellScriptBin;
+
       shell = with pkgs; mkShell {
-        # Insert Packages Here
-        packages = [];
+        packages = [
+          # Insert packages here
+
+          # Insert shell aliases here
+          (mkScript "hello" ''echo $SOME_ENV_VAR'')
+        ];
+
+        shellHook = ''
+          export SOME_ENV_VAR="Hello World"
+        ''
       };
     in {
       devShells.default = shell;
