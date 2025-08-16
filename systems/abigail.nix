158ee180714920dd689d04e555e7ba17bee03255
Brandon Blaylock <brandon@null.pub>
2025-08-15 18:09:17 -0700

    feat: add reverse proxy for id.null.pub

--- systems/abigail.nix
+++ systems/abigail.nix
@@ -82,6 +82,22 @@
         };
       };
 
+      "id.null.pub" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://[::1]:${toString config.services.pocket-id.port}";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+          extraConfig = ''
+            client_max_body_size 50000M;
+            proxy_read_timeout   600s;
+            proxy_send_timeout   600s;
+            send_timeout         600s;
+          '';
+        };
+      };
+
       "immich.null.pub" = {
         enableACME = true;
         forceSSL = true;
